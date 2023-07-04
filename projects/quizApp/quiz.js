886a1281a6c19706d5f9ae14dcccfba65759c1e9
Arman091 <rasoolarman7@gmail.com>
2023-07-04 12:37:03 +0530

    "sports quiz app"

---
+++ projects/quizApp/quiz.js
@@ -0,0 +1,74 @@
+// an Array of set Questions and answers
+
+let questions = [
+  {
+    question: "Which country won the FIFA World Cup in 2018?",
+    options: ["Brazil", "Germany", "France", "Spain"],
+    answer: 2,
+  },
+  {
+    question: "In which sport would you perform a slam dunk?",
+    options: ["Basketball", "Tennis", "Golf", "Swimming"],
+    answer: 0,
+  },
+  {
+    question: "Which country is famous for its passion for cricket?",
+    options: ["England", "India", "Australia", "South Africa"],
+    answer: 1,
+  },
+  {
+    question: "Which athlete has the most Olympic gold medals?",
+    options: [
+      "Usain Bolt",
+      "Michael Phelps",
+      "Serena Williams",
+      "Simone Biles",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What is the national sport of Canada?",
+    options: ["Hockey", "Baseball", "Soccer", "Rugby"],
+    answer: 0,
+  },
+  {
+    question: "Which sport uses a shuttlecock?",
+    options: ["Badminton", "Table Tennis", "Volleyball", "Tennis"],
+    answer: 0,
+  },
+  {
+    question: "Which boxer was known as 'The Greatest'?",
+    options: [
+      "Muhammad Ali",
+      "Mike Tyson",
+      "Floyd Mayweather Jr.",
+      "Manny Pacquiao",
+    ],
+    answer: 0,
+  },
+];
+
+let index = 0; // to keep track of Question
+
+// function to display Question and Set Answer
+
+function setQuestion() {
+  let question = document.getElementById("question-text");
+  let options = document.getElementById("answer-options");
+  question.innerText = questions[index].question;
+  options.innerHTML = `
+    <li class="option">${questions[index].options[0]}</li>
+    <li class="option" >${questions[index].options[1]}</li>
+    <li class="option">${questions[index].options[2]}</li>
+    <li class="option">${questions[index].options[3]}</li>
+    `;
+}
+
+setQuestion(index); // initial question SetUp;
+
+// function to get Next Question
+
+function nextQuestion() {
+  index = index + 1;
+  setQuestion(index);
+}
