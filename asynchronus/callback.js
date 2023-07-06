f2a4eab0b8745e68ae9932d0915ad4920f8db5f4
Arman091 <rasoolarman7@gmail.com>
2023-07-06 15:57:06 +0530

    "closures"

---
+++ asynchronus/callback.js
@@ -0,0 +1,20 @@
+/* 
+1. what is callback
+
+2. sync  vs asynchronus;
+
+3. blocking the main thread
+
+4. event listeners
+
+5. garbage collection
+*/
+
+
+//SETTIMEOUT AS CALLBACK FUNCTION
+
+console.log("arman");
+setTimeout(() => {
+    console.log('rasool')
+}, 1000);
+
