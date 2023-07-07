6485ece6e747dc5dd8693ac1fedfbe5a07af6978
Arman091 <rasoolarman7@gmail.com>
2023-07-07 20:32:42 +0530

    "doom pyramid"

---
+++ asynchronus/doompyra.js
@@ -0,0 +1,31 @@
+const heading1 = document.getElementsByClassName("grid-item")[1];
+const heading2 = document.getElementsByClassName("grid-item")[2];
+const heading3 = document.getElementsByClassName("grid-item")[3];
+const heading4 = document.getElementsByClassName("grid-item")[4];
+const heading5 = document.getElementsByClassName("grid-item")[5];
+
+function changeColor(heading, text, color, onSuccesNextstep,errorHandler) {
+  setTimeout(() => {
+    heading.textContent = text;
+    heading.style.color = color;
+
+    if (onSuccesNextstep) {
+      onSuccesNextstep();
+    }
+    else {
+      errorHandler();
+    }
+  }, 1000);
+}
+
+changeColor(heading1, "step 1", "red", () => {
+    changeColor(heading2, "step 2", "green", () => {
+        changeColor(heading3, "step 3", "violet", () => {
+          changeColor(heading4, "step 4", "blue", () => {
+            changeColor(heading5, "step 5", "grey", () => {
+                 console.log("end of program")
+            }, () => {console.log("error performing action") })
+           },()=>{console.log("error performing operation");}) 
+        },()=>{console.log("error performing operation");})
+    },()=>{console.log("error performing operation");})
+},()=>{console.log("error performing operation");});
