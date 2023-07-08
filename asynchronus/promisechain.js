be5856d33b3f3d112ed6e22162b1d0a9bb552c6e
Arman091 <rasoolarman7@gmail.com>
2023-07-08 11:33:54 +0530

    "promise chaining"

---
+++ asynchronus/promisechain.js
@@ -0,0 +1,7 @@
+let stock = {
+    fruits: ["apple", "grapes", "bananas", "strawberry"],
+    liquids: ["water", "ice"],
+    holder: ["cone", "cup", "sticks"],
+    tapping: ["chochlate", "peanuts"]
+};
+
