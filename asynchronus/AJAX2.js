347f067f57b3d0dd06b2278f341b1b3d44f9543a
Arman091 <rasoolarman7@gmail.com>
2023-07-12 17:48:47 +0530

    "update"

--- asynchronus/AJAX2.js
+++ asynchronus/AJAX2.js
@@ -11,20 +11,33 @@ const xhr = new XMLHttpRequest();
  *  2. Header_recieved:- send() method has been called and server has recieved the REQUEST and RESPONSE-HEADERS
  *                       availabe.
  *
- *  3.
+ *  3.loading-> partial data may be available.
+ *
+ * 4. Done-> you can acces complete data.
  *
  */
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4 && xhr.status === 200) {
     // Code to handle the successful response
-    console.log(xhr.responseText);
+
+    dataHandler(xhr.responseText);
   }
 };
-
+async function dataHandler(usersobject) {
+  let data = await JSON.parse(usersobject);
+  let users = data.slice(0, 12); // Create a new array with the first 10 items
+  let element = document.getElementsByClassName("grid-continer")[0];
+  users.forEach((user) => {
+    let child = document.createElement("div");
+    child.className = "grid-item";
+    child.innerHTML = `User Id: ${user.id}`;
+    element.appendChild(child);
+  });
+}
 
 // open methods to open request
 /**
 The third parameter in the xhr.open() method represents whether the request should be asynchronous or not. In your example, the value true is passed as the third parameter, indicating that the request should be asynchronous.
  */
-xhr.open("GET", url,true);
+xhr.open("GET", url, true);
 xhr.send();
