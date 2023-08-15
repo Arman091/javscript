30b642f01756725d73302fc5dba3e9a9df08d34f
Arman091 <rasoolarman7@gmail.com>
2023-08-15 20:01:09 +0530

    "update"

--- asynchronus/FetchApi.js
+++ asynchronus/FetchApi.js
@@ -1,13 +1,14 @@
 // FETCH API ALLOWES TO MAKE HTTP REQUEST AND HANDLE RESPONSE EASILY;
 
 // IT RETURNS A PROMOSE OBJECT THAT CAN BE HANDLES ACCORDINGLY;
-const url = "https://jsonplaceholder.typicode.com/posts";
+// const url = "https://jsonplaceholder.typicode.com/posts";
+const url = "https://floralcart.onrender.com/products";
 fetch(url)
     .then((response) => {
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
-        return response.json();
+        return response
     
     /**
      * The response.json() method in the Fetch API is used to extract the JSON body content from the HTTP response.
---
+++ logicalQues/pattern.js
@@ -0,0 +1,7 @@
+
+for (let row = 0; row <= 5; row++){
+    for (let col = 0; col <= row; col++){
+        console.log("X")
+    }
+    console.log("\n")
+}
